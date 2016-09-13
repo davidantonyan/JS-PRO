a0111de567ff0f069c27f614b0f6aba1d50d8438
David <davidantonyan1981@hotmail.com>
2016-09-14 00:40:59 +0400

    test commits functions

--- scripts/functions.js
+++ scripts/functions.js
@@ -41,3 +41,40 @@ function makeArray(){
 
 // call makeArray
 makeArray('a','1','1000'); // ['a','1','1000']
+
+function walkDOM(node, func){
+	func(node);
+
+	node = node.firstChild;
+	while(node){
+		walkDOM(node, func);
+		node = node.nextSiblings;
+	}
+}
+
+// Bad Example
+
+var add_handlers_bad = function(nodes){
+	var i;
+	for(i = 0; i < nodes.length; i++){
+		nodes[i].onclick = function(e){
+			alert(i);
+		}
+	}
+}
+
+// end Bad Example
+
+// Better Example
+
+var add_handlers_better = function(nodes){
+	var i;
+	
+	for(i = 0; i < nodes.length; i++){
+		nodes[i].onclick = function(i){
+			return function(e){
+				alert(e);
+			};
+		}(i);
+	}
+}
